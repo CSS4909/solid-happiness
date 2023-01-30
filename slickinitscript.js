c50962c959b45e6072954c8ec2551aeae370acd2
CSS4909 <123831334+CSS4909@users.noreply.github.com>
2023-01-30 12:29:26 -0500

    Update slickinitscript.js

--- slickinitscript.js
+++ slickinitscript.js
@@ -1,7 +1,6 @@
  <script type="text/javascript">
     $(document).ready(function(){
-      $('.your-class').slick({
-        setting-name: setting-value
+      $('.single-item').slick({
       });
     });
   </script>
